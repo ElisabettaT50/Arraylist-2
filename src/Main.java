153e0b650eee95163918f9d3c619c69bf1708597
ElisabettaT50 <150373783+ElisabettaT50@users.noreply.github.com>
2023-12-19 17:05:12 +0100

    Update Main.java
    consegna corretta

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ import java.util.List;
 /*
 Creare una classe Student che accenti nel costruttore il parametro name (String e age Int)
 In questa classe avere i metodi pubblici per ottenere le informazioni.
-Creare un ArrayList con n elementi e stamparlo in console.
-Aggiungere 4 elementi alla collezione e stampare la collezione aggiornata in console
+Creare un ArrayList con 12 elementi e stamparlo in console.
+mettere in ordine la collezione e stampare il risultato
  */
 public class Main {
     public static void main(String[] args) {
